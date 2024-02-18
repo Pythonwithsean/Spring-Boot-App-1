6a5e09edb8c5e78d568899a8d4f5be6c42ce1052
Pythonwithsean <Lunafcs@protonmail.com>
2024-02-18 16:32:26 +0000

    Initial Comit

---
+++ src/main/java/com/seanapi/example/Routes/C.java
@@ -0,0 +1,19 @@
+package com.seanapi.example.Routes;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class C {
+
+    @GetMapping("/home")
+    public String Home(){
+        return "Home";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "Login";
+    }
+}
---
+++ src/main/java/com/seanapi/example/SecurityConfiguration.java
@@ -0,0 +1,7 @@
+package com.seanapi.example;
+
+
+
+public class SecurityConfiguration{
+
+}
